01764484eaf1c5a86841480cf72267efb7b0b79b
Alexander Attar <alexanderattar@gmail.com>
2013-12-02 15:40:52 -0500

    added logic to prevent the same track from being played again

--- static/js/hifi.js
+++ static/js/hifi.js
@@ -26,36 +26,13 @@ function checkKey(e) {
     }
     if (e.keyCode == '76') {  // L key
         if (authorized) {
+            console.log('L Key. Liking track');
             likeSound(trackIds[trackIds.length - 1]);  // current track
         } else {
             console.log('Not connected to SoundCloud');
         }
     }
     // TODO - Add Play, Pause, and Volume controls
-}
-
-function createSpinnder() { // ridin' spinners
-var opts = {
-  lines: 7, // The number of lines to draw
-  length: 0, // The length of each line
-  width: 10, // The line thickness
-  radius: 10, // The radius of the inner circle
-  corners: 1, // Corner roundness (0..1)
-  rotate: 0, // The rotation offset
-  direction: 1, // 1: clockwise, -1: counterclockwise
-  color: '#000', // #rgb or #rrggbb or array of colors
-  speed: 1, // Rounds per second
-  trail: 60, // Afterglow percentage
-  shadow: false, // Whether to render a shadow
-  hwaccel: false, // Whether to use hardware acceleration
-  className: 'spinner', // The CSS class to assign to the spinner
-  zIndex: 2e9, // The z-index (defaults to 2000000000)
-  top: 'auto', // Top position relative to parent in px
-  left: 'auto' // Left position relative to parent in px
-};
-    var target = document.getElementById('loading');
-    var s = new Spinner(opts).spin(target);
-    spinner = s;
 }
 
 $(document).ready(function() {
@@ -137,9 +114,7 @@ function getTracks(selection) {
     if (soundcloud_url) {
         try {  // sometimes the request to the SoundCloud API bombs out. this handles that
             SC.get(soundcloud_url, function(tracks) {
-                // get a random track from the sounds returned
-                var random = Math.floor(Math.random() * tracks.length);
-                var soundcloud_url = tracks[random].permalink_url;
+                var soundcloud_url = getNext(tracks);
                 setupWidget(soundcloud_url, selection);
             });
         } catch(e) {
@@ -154,10 +129,7 @@ function getTracks(selection) {
             SC.get('/tracks', {
                 genres: selection, limit: 200, offset: Math.floor(Math.random() * 7999)
             }, function(tracks) {
-                // get a random track from the 200 returned
-                var random = Math.floor(Math.random() * 199);
-                var soundcloud_url = tracks[random].permalink_url;
-                trackIds.push(tracks[random].id);  // keep this so we can get the previous track
+                var soundcloud_url = getNext(tracks);
                 setupWidget(soundcloud_url);
             });
         } catch(e) {
@@ -168,6 +140,21 @@ function getTracks(selection) {
     }
 }
 
+/* Prevents the same track from playing */
+function getNext(tracks) {
+    var random = Math.floor(Math.random() * tracks.length);
+    var trackId = tracks[random].id;
+    // while the track is in the list of played tracks search for another track
+    while ($.inArray(trackId, trackIds) > -1)  {
+        console.log('That track has already been played. Searching for another one');
+        random = Math.floor(Math.random() * tracks.length);
+        trackId = tracks[random].id;
+    }
+    trackIds.push(trackId);  // keep this so we can get the previous track
+    return tracks[random].permalink_url;
+}
+
+/* Look in the track ID history and play the previously played track */
 function getPrevious(trackId) {
     var soundcloud_url = '/tracks/' + trackId;
     SC.get(soundcloud_url, 'allow_redirects=False', function(track) {
@@ -184,9 +171,7 @@ function likeSound(trackId) {
 
 /* Parses the images returned from Tumblr */
 function getImages(data, images) {
-
     for (var i = 0; i < data.response.length; i++) {
-
         // get the image url from the post
         if (data.response[i].photos){
             images.push(data.response[i].photos[0].original_size.url);
@@ -196,7 +181,6 @@ function getImages(data, images) {
 
 /* Contains the main logic for requesting images from Tumblr */
 function tumble(selection) {
-
     // images = [];  // reset images
     $('#image-holder').empty();  // clear out images in the DOM
     embedImages(images, selection);
@@ -234,7 +218,6 @@ function tumble(selection) {
 
 /* Useful for shuffling images to create different ordering for each play */
 function shuffle(array) {
-
     var counter = array.length, temp, index;
 
     // While there are elements in the array
@@ -252,7 +235,6 @@ function shuffle(array) {
 
 /* Injects the images into the DOM */
 function embedImages(images, selection) {
-
     // randomize the order of the images and limit it to the first 300 images
     images = shuffle(images).slice(0, 300);
 
@@ -260,14 +242,12 @@ function embedImages(images, selection) {
         var imageId = 'image' + i;
         $('#image-holder').append('<div id="' + imageId + '" class="row images text-center hide"><img src="'+ images[i] + '"></div>');
     }
-
     // once the images are loaded we can get the audio and set the image transition points
     getTracks(selection);
 }
 
 /* Handles SoundCloud widget events, and contains logic for transitioning between images */
 function setupWidget(soundcloud_url, selection) {
-
     // the total duration of 100 percent of the audio is divided by the amount of images
     // and the value is used to create the transiton points that are hit by an incrementing
     // index as the sound progresses over time
